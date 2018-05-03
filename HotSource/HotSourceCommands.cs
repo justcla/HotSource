dfb880efaed3375a083aee5ed2494e79a51beff4
Justin Clareburt <justcla@microsoft.com>
2018-05-03 02:11:51 -0700

    Implemented some global commands.

--- HotSource/HotSourceCommands.cs
+++ HotSource/HotSourceCommands.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.ComponentModel.Design;
-using System.Globalization;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using EnvDTE;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
 using Task = System.Threading.Tasks.Task;
 
 namespace HotSource
@@ -15,9 +14,19 @@ namespace HotSource
     internal sealed class HotSourceCommands
     {
         /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static HotSourceCommands Instance { get; private set; }
+
+        /// <summary>
         /// VS Package that provides this command, not null.
         /// </summary>
         private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider { get { return this.package; } }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="HotSourceCommands"/> class.
@@ -33,36 +42,6 @@ namespace HotSource
             AddGlobalCommandHandlers(commandService);
         }
 
-        private void AddGlobalCommandHandlers(OleMenuCommandService commandService)
-        {
-            // Add command handlers for global commands
-            commandService.AddCommand(CreateMenuCommand(CommandIDs.CommitCmdId));
-            commandService.AddCommand(CreateMenuCommand(CommandIDs.PushCmdId));
-            commandService.AddCommand(CreateMenuCommand(CommandIDs.PullCmdId));
-            commandService.AddCommand(CreateMenuCommand(CommandIDs.FetchCmdId));
-            commandService.AddCommand(CreateMenuCommand(CommandIDs.SyncCmdId));
-            commandService.AddCommand(CreateMenuCommand(CommandIDs.PendingChangesCmdId));
-            commandService.AddCommand(CreateMenuCommand(CommandIDs.UnpushedCommitsCmdId));
-            commandService.AddCommand(CreateMenuCommand(CommandIDs.NewBranchCmdId));
-            commandService.AddCommand(CreateMenuCommand(CommandIDs.ManageBranchesCmdId));
-            commandService.AddCommand(CreateMenuCommand(CommandIDs.ShowBranchHistoryCmdId));
-        }
-
-        private MenuCommand CreateMenuCommand(int commandId)
-        {
-            return new MenuCommand(this.Execute, new CommandID(CommandIDs.HotSourceCommandSetGuid, commandId));
-        }
-
-        /// <summary>
-        /// Gets the instance of the command.
-        /// </summary>
-        public static HotSourceCommands Instance { get; private set; }
-
-        /// <summary>
-        /// Gets the service provider from the owner package.
-        /// </summary>
-        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider { get { return this.package; } }
-
         /// <summary>
         /// Initializes the singleton instance of the command.
         /// </summary>
@@ -77,21 +56,72 @@ namespace HotSource
             Instance = new HotSourceCommands(package, commandService);
         }
 
-        /// <summary>
-        /// This function is the callback used to execute the command when the menu item is clicked.
-        /// See the constructor to see how the menu item is associated with this function using
-        /// OleMenuCommandService service and MenuCommand class.
-        /// </summary>
-        /// <param name="sender">Event sender.</param>
-        /// <param name="e">Event args.</param>
-        private void Execute(object sender, EventArgs e)
+        private void AddGlobalCommandHandlers(OleMenuCommandService commandService)
+        {
+            // Add command handlers for global commands
+            commandService.AddCommand(CreateMenuCommand(CommandIDs.CommitCmdId, this.ExecCommit));
+            commandService.AddCommand(CreateMenuCommand(CommandIDs.PushCmdId, this.GoToSyncWindow));
+            commandService.AddCommand(CreateMenuCommand(CommandIDs.PullCmdId, this.GoToSyncWindow));
+            commandService.AddCommand(CreateMenuCommand(CommandIDs.FetchCmdId, this.GoToSyncWindow));
+            commandService.AddCommand(CreateMenuCommand(CommandIDs.SyncCmdId, this.GoToSyncWindow));
+            commandService.AddCommand(CreateMenuCommand(CommandIDs.PendingChangesCmdId, this.GoToPendingChanges));
+            commandService.AddCommand(CreateMenuCommand(CommandIDs.UnpushedCommitsCmdId, this.GoToSyncWindow));
+            commandService.AddCommand(CreateMenuCommand(CommandIDs.NewBranchCmdId, this.ExecCreateBranch));
+            commandService.AddCommand(CreateMenuCommand(CommandIDs.ManageBranchesCmdId, this.GoToBranchesWindow));
+            commandService.AddCommand(CreateMenuCommand(CommandIDs.ShowBranchHistoryCmdId, this.ExecViewBranchHistory));
+        }
+
+        private MenuCommand CreateMenuCommand(int commandId, EventHandler eventHandler)
+        {
+            return new MenuCommand(eventHandler, new CommandID(CommandIDs.HotSourceCommandSetGuid, commandId));
+        }
+
+        private void ExecCommit(object sender, EventArgs e)
+        {
+            ExecuteDTECommand("View.PendingChanges");
+        }
+        private void GoToPendingChanges(object sender, EventArgs e)
+        {
+            ExecuteDTECommand("View.PendingChanges");
+        }
+        private void ExecViewBranchHistory(object sender, EventArgs e)
+        {
+            //DoViewBranchHistory();
+            ExecuteDTECommand("Team.Git.ViewHistory");
+        }
+        private void GoToSyncWindow(object sender, EventArgs e)
+        {
+            ExecuteDTECommand("Vew.UnpublishedCommits");
+        }
+
+        private void ExecCreateBranch(object sender, EventArgs e)
+        {
+            ExecuteDTECommand("Team.Git.CreateBranch");
+        }
+
+        private void GoToBranchesWindow(object sender, EventArgs e)
+        {
+            ExecuteDTECommand("Team.Git.ManageBranches");
+        }
+
+        //---------Helper methods  ------------------------------------
+
+        private async Task ExecuteDTECommand(string commandName)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            DTE dte = await package.GetServiceAsync((typeof(DTE))) as DTE;
+            dte.ExecuteCommand(commandName);
+        }
+
+        private void ShowStandardMessageBox()
         {
             ThreadHelper.ThrowIfNotOnUIThread();
             string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
             string title = "Hot Source";
 
             // Show a message box to prove we were here
-            VsShellUtilities.ShowMessageBox( this.package, message, title,
+            VsShellUtilities.ShowMessageBox(this.package, message, title,
                 OLEMSGICON.OLEMSGICON_INFO, OLEMSGBUTTON.OLEMSGBUTTON_OK, OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
         }
     }
